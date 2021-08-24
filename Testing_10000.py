fd09d11473b48bc3803e76e911773daf9cc4dab2
Delberin Ali <delberinali92@gmail.com>
2021-08-24 17:25:09 +0200

    added the updated submodule

--- Testing_10000.py
+++ Testing_10000.py
@@ -30,7 +30,7 @@ from matplotlib.backends.backend_pdf import PdfPages, FigureCanvasPdf, PdfFile
 import warnings
 warnings.filterwarnings("ignore")
 
-path = 'R:\\radio\\2002-20yy_Callisto\\2017\\09\\06'
+# path = 'R:\\radio\\2002-20yy_Callisto\\2017\\09\\06'
 
 # Source: https://gist.github.com/salotz/8b4542d7fe9ea3e2eacc1a2eef2532c5
 
@@ -63,7 +63,7 @@ cursor = connection.cursor()
 
 cursor.execute("""select * from (
                                  select ROW_NUMBER() OVER (partition by instrument_name order by id)
-                                 as row_num, ecallisto2017.* FROM ecallisto2017
+                                 as row_num, ecallisto.* FROM ecallisto
                                  ) t
                                  where row_num <=10
                                  order by instrument_name""")
@@ -72,12 +72,12 @@ cursor.execute("""select * from (
 
 my_colormap = matplotlib.colors.LinearSegmentedColormap.from_list("myColorMap", plot_config.COLORMAP / 255)
 
-with PdfPages('C:\\Users\\delbe\\OneDrive\\Desktop\\eCallisto_validation\\Plot_PDF.pdf') as pdf:
+with PdfPages('Images.pdf') as pdf:
     
     for file in cursor.fetchall():
-
-        full_path = os.path.join(path, file[2]) # 2 is the index of file_name in the cursor
-        spec = CallistoSpectrogram.read(full_path)
+       # full_path = os.path.join(path, file[2]) # 2 is the index of file_name in the cursor
+        spec = CallistoSpectrogram.read(file[2])
+        print(file[2])
         fig1, axs1 = plt.subplots(1, 4, figsize=(25,5))
         ax1 = spec.plot(cmap=my_colormap, colorbar=None)
         ax1.title.set_text("Original Data")
@@ -87,7 +87,7 @@ with PdfPages('C:\\Users\\delbe\\OneDrive\\Desktop\\eCallisto_validation\\Plot_P
         spec2 = spec.subtract_bg("constbacksub", "elimwrongchannels")
         fig2 = plt.subplots(1, 4, figsize=(25,5))
         ax2 = spec2.plot(cmap=my_colormap, colorbar=None, vmin=-5, vmax=5)
-        ax2.title.set_text("Bg_cbs_rfi")
+        ax2.title.set_text("Background subtracted")
         plt.close()
 
         # Third column, subtract_bg_sliding_window
@@ -95,7 +95,7 @@ with PdfPages('C:\\Users\\delbe\\OneDrive\\Desktop\\eCallisto_validation\\Plot_P
                                  amount=0.05, change_points=True)
         fig3 = plt.figure(figsize=(25,5))
         ax3 = spec3.plot(cmap=my_colormap, colorbar=None, vmin=-5, vmax=5)
-        ax3.title.set_text("Bg_sub_sliding_rfi")
+        ax3.title.set_text("Gliding background subtracted (window=800)")
         plt.close()
 
         # Fourth column, Histograms
@@ -103,8 +103,8 @@ with PdfPages('C:\\Users\\delbe\\OneDrive\\Desktop\\eCallisto_validation\\Plot_P
 
         data_hist4 = np.absolute(spec3.data.flatten())
         fig4, ax4 = plt.subplots(figsize=(25,5))
-        ax4.hist(data_hist3 ,histtype='step',range= (0, 10), bins= 40, label='Bg_cbs_rfi')
-        ax4.hist(data_hist4 ,histtype='step',range= (0, 10), bins= 40, label='Bg_sub_sliding_rfi')
+        ax4.hist(data_hist3 ,histtype='step',range= (0, 10), bins= 40, label='Background subtracted')
+        ax4.hist(data_hist4 ,histtype='step',range= (0, 10), bins= 40, label='Gliding background subtracted')
         ax4.title.set_text("Histograms")
         plt.legend()
         plt.close()
@@ -118,6 +118,19 @@ with PdfPages('C:\\Users\\delbe\\OneDrive\\Desktop\\eCallisto_validation\\Plot_P
         move_axes(fig_target, ax2, axB)
         move_axes(fig_target, ax3, axC)
         move_axes(fig_target, ax4, axD)
+
+        ax1.set_xlabel('Time[UT]')
+        ax1.set_ylabel('Frequency[MHz]')
+
+        ax2.set_xlabel('Time[UT]')
+        ax2.set_ylabel('Frequency[MHz]')
+
+        ax3.set_xlabel('Time[UT]')
+        ax3.set_ylabel('Frequency[MHz]')
+
+        ax4.set_xlabel('Pixel value')
+        ax4.set_ylabel('Number of pixels')
+
         plt.show()
 
         pdf.savefig(fig_target)
