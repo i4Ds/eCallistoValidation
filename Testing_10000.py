cfb9c527875b3b9b3b29f18c48cc3783aa87e4f9
Delberin Ali <61394570+Delberin-Ali@users.noreply.github.com>
2021-08-25 17:56:02 +0200

    - Added count to get the 10000 Images

--- Testing_10000.py
+++ Testing_10000.py
@@ -48,10 +48,7 @@ def move_axes(fig, ax_source, ax_target):
     fig.add_subplot(ax_source)
     
     plt.close(old_fig)
-    
 
-    
-    
 # Connection between Jupyter notebook and Postgres
 connection = psycopg2.connect(user="postgres",
                               password="ecallistohackorange",
@@ -69,70 +66,72 @@ cursor.execute("""select * from (
                                  order by instrument_name""")
 
 
-
 my_colormap = matplotlib.colors.LinearSegmentedColormap.from_list("myColorMap", plot_config.COLORMAP / 255)
 
-with PdfPages('Images.pdf') as pdf:
-    
+count = 0
+
+with PdfPages('Images_pdf.pdf') as pdf:
+
     for file in cursor.fetchall():
-        spec = CallistoSpectrogram.read(file[2])
-        print(file[2])
-        fig1, axs1 = plt.subplots(1, 4, figsize=(25,5))
-        ax1 = spec.plot(cmap=my_colormap, colorbar=None)
-        ax1.title.set_text("Original Data")
-        plt.close()
+        if count < 10000 :
 
-        # Second column, Constbacksub + elimwrongchannels
-        spec2 = spec.subtract_bg("constbacksub", "elimwrongchannels")
-        fig2 = plt.subplots(1, 4, figsize=(25,5))
-        ax2 = spec2.plot(cmap=my_colormap, colorbar=None, vmin=-5, vmax=5)
-        ax2.title.set_text("Background subtracted")
-        plt.close()
+            spec = CallistoSpectrogram.read(file[2])
+            fig1, axs1 = plt.subplots(1, 4, figsize=(28, 7))
+            ax1 = spec.plot()
+            ax1.title.set_text("Original Data")
+            plt.close()
 
-        # Third column, subtract_bg_sliding_window
-        spec3 = spec.subtract_bg("subtract_bg_sliding_window", window_width=800, affected_width=1,
-                                 amount=0.05, change_points=True)
-        fig3 = plt.figure(figsize=(25,5))
-        ax3 = spec3.plot(cmap=my_colormap, colorbar=None, vmin=-5, vmax=5)
-        ax3.title.set_text("Gliding background subtracted (window=800)")
-        plt.close()
+            # Second column, Constbacksub + elimwrongchannels
+            spec2 = spec.subtract_bg("constbacksub", "elimwrongchannels")
+            fig2 = plt.subplots(1, 4, figsize=(28, 7))
+            ax2 = spec2.plot(vmin=-5, vmax=5)
+            ax2.title.set_text("Background subtracted")
+            plt.close()
 
-        # Fourth column, Histograms
-        data_hist3 = np.absolute(spec2.data.flatten())
+            # Third column, subtract_bg_sliding_window
+            spec3 = spec.subtract_bg("subtract_bg_sliding_window", window_width=800, affected_width=1,
+                                     amount=0.05, change_points=True)
+            fig3 = plt.figure(figsize=(28, 7))
+            ax3 = spec3.plot(vmin=-5, vmax=5)
+            ax3.title.set_text("Gliding background subtracted (window=800)")
+            plt.close()
 
-        data_hist4 = np.absolute(spec3.data.flatten())
-        fig4, ax4 = plt.subplots(figsize=(25,5))
-        ax4.hist(data_hist3 ,histtype='step',range= (0, 10), bins= 40, label='Background subtracted')
-        ax4.hist(data_hist4 ,histtype='step',range= (0, 10), bins= 40, label='Gliding background subtracted')
-        ax4.title.set_text("Histograms")
-        plt.legend()
-        plt.close()
+            # Fourth column, Histograms
+            data_hist3 = np.absolute(spec2.data.flatten())
 
-        # Plot final plot by moving axes to the figure
-        fig_target, (axA, axB, axC, axD) = plt.subplots(1, 4, figsize=(30,5))
-        plt.suptitle(fig1._suptitle.get_text())
+            data_hist4 = np.absolute(spec3.data.flatten())
+            fig4, ax4 = plt.subplots(figsize=(28, 7))
+            ax4.hist(data_hist3 ,histtype='step',range= (0, 10), bins= 40, label='Background subtracted')
+            ax4.hist(data_hist4 ,histtype='step',range= (0, 10), bins= 40, label='Gliding background subtracted')
+            ax4.title.set_text("Histograms")
+            plt.legend()
+            plt.close()
 
+            # Plot final plot by moving axes to the figure
+            fig_target, (axA, axB, axC, axD) = plt.subplots(1, 4, figsize=(30,7))
+            plt.suptitle(fig1._suptitle.get_text())
 
-        move_axes(fig_target, ax1, axA)
-        move_axes(fig_target, ax2, axB)
-        move_axes(fig_target, ax3, axC)
-        move_axes(fig_target, ax4, axD)
+            move_axes(fig_target, ax1, axA)
+            move_axes(fig_target, ax2, axB)
+            move_axes(fig_target, ax3, axC)
+            move_axes(fig_target, ax4, axD)
 
-        ax1.set_xlabel('Time[UT]')
-        ax1.set_ylabel('Frequency[MHz]')
+            ax1.set_xlabel('Time[UT]')
+            ax1.set_ylabel('Frequency[MHz]')
 
-        ax2.set_xlabel('Time[UT]')
-        ax2.set_ylabel('Frequency[MHz]')
+            ax2.set_xlabel('Time[UT]')
+            ax2.set_ylabel('Frequency[MHz]')
 
-        ax3.set_xlabel('Time[UT]')
-        ax3.set_ylabel('Frequency[MHz]')
+            ax3.set_xlabel('Time[UT]')
+            ax3.set_ylabel('Frequency[MHz]')
 
-        ax4.set_xlabel('Pixel value')
-        ax4.set_ylabel('Number of pixels')
+            ax4.set_xlabel('Pixel value')
+            ax4.set_ylabel('Number of pixels')
 
-        plt.show()
+            plt.show()
 
-        pdf.savefig(fig_target)
-        plt.close()
+            pdf.savefig(fig_target)
+            plt.close()
 
-print("Finished plotting!")
+            count += 1
+    print("Finished plotting!")
