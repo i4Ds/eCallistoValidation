2833291dfa3ab10bd58218ecf2327e96ceb69f5b
Delberin Ali <delberinali92@gmail.com>
2021-08-25 09:23:52 +0200

    update the file to save the metada into the DB

--- save_to_sql.py
+++ save_to_sql.py
@@ -32,37 +32,33 @@ import warnings
 warnings.filterwarnings("ignore")
 
 
-
 def __to_timestamp(date_string, time_string):
-    
-                sixty_seconds = int(time_string[6:8]) == 60
-                sixty_minutes = int(time_string[3:5]) == 60
-                twentyfour_hours = int(time_string[:2]) == 24
-                
-                # replacing  24 to 00 
-                if sixty_seconds :
-                    time_string = time_string[:6] + '59' + time_string[8:]
-                if sixty_minutes :
-                    time_string = time_string[:3] + '59' + time_string[5:]
-                if twentyfour_hours :
-                    time_string = '23' + time_string[2:]
-                if re.findall("\.\d+", time_string):
-                    time_string = time_string[:-4]
-                    
-                # lost time     
-                ts = datetime.datetime.strptime(
-                    '%s %s' % (date_string, time_string), '%Y/%m/%d %H:%M:%S')                  
-                ts += datetime.timedelta(hours = int(twentyfour_hours),
-                                         minutes = int(sixty_minutes),
-                                         seconds = int(sixty_seconds))
+
+    sixty_seconds = int(time_string[6:8]) == 60
+    sixty_minutes = int(time_string[3:5]) == 60
+    twentyfour_hours = int(time_string[:2]) == 24
+
+    # replacing  24 to 00
+    if sixty_seconds:
+        time_string = time_string[:6] + '59' + time_string[8:]
+    if sixty_minutes:
+        time_string = time_string[:3] + '59' + time_string[5:]
+    if twentyfour_hours:
+        time_string = '23' + time_string[2:]
+    if re.findall("\.\d+", time_string):
+        time_string = time_string[:-4]
+
+    # lost time
+    ts = datetime.datetime.strptime(
+        '%s %s' % (date_string, time_string), '%Y/%m/%d %H:%M:%S')
+    ts += datetime.timedelta(hours=int(twentyfour_hours),
+                             minutes=int(sixty_minutes),
+                             seconds=int(sixty_seconds))
+
+    return ts
                 
 
-                return ts
-                
-                
-                
 path = 'R:\\radio\\2002-20yy_Callisto\\2017\\09'
-
 engine = create_engine("postgresql+psycopg2://" + 'postgres' + ":" + 'ecallistohackorange' + "@" + 'localhost' + "/" + 'validation')
 
 df = 0
@@ -70,9 +66,9 @@ df = 0
 for root, dirs, files in os.walk(path):
     for name in files:
         if name.endswith('.fit.gz'):
-            instruments = os.path.join(root, name)
+            full_path = os.path.join(root, name)
             
-            hdulist = astropy.io.fits.open(instruments)
+            hdulist = astropy.io.fits.open(full_path)
             
             instrument_name = hdulist[0].header['INSTRUME'] 
             date_obs = hdulist[0].header['DATE-OBS'] 
@@ -86,18 +82,19 @@ for root, dirs, files in os.walk(path):
             
             # creating dataframe in pandas
             
-            data={
-                'file_name':[name],
-                  'instrument_name':[instrument_name],
-                  'start_time':[start_time] ,
-                  'end_time':[end_time],
-                  'std' : [None]
+            data = {
+                'path': [full_path],
+                'file_name': [name],
+                'instrument_name': [instrument_name],
+                'start_time': [start_time],
+                'end_time': [end_time],
+                'std': [None]
                   }
              
-            data_frame = pd.DataFrame(data, index = [df]) 
+            data_frame = pd.DataFrame(data, index=[df])
             
             # connection between pandas and sql 
-            data_frame.to_sql('ecallisto_2017', con = engine,if_exists = 'append', chunksize = 50000, index=False)
+            data_frame.to_sql('validation_data', con=engine, if_exists='append', chunksize=10000000, index=False)
                        
             df = df + 1
                 
