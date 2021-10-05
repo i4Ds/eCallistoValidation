c0f06a08a79c180a31daf18138b82605b9111e05
Delberin Ali <61394570+Delberin-Ali@users.noreply.github.com>
2021-10-05 17:15:05 +0200

    - reStructuredText Docstring Format

--- calculate_sd_snr.py
+++ calculate_sd_snr.py
@@ -2,41 +2,35 @@ from packages.modules import *
 from packages.main import *
 
 
+def get_connection():
+    global connection, cursor
+    connection = psycopg2.connect(host=test_config.DB_HOST,
+                                  database=test_config.DB_DATABASE,
+                                  user=test_config.DB_USER,
+                                  port=test_config.DB_PORT,
+                                  password=test_config.DB_PASSWORD)
+    cursor = connection.cursor()
+
+    return connection, cursor
+
+
 def get_cursor():
-    """
-    Select all data from the table "validation_data" to update the Std and Snr values.
-    Returns
-    -------
-    A query of the Table.
-    """
-    cursor.execute("""SELECT * from validation_data WHERE std is null ORDER BY id""")
-
-
-def update_values():
-
-    sql_update_query = f"""UPDATE validation_data SET std = {std_data}, snr = {snr_data} where id = {index[0]} """
-    cursor.execute(sql_update_query)
-    print("Table after Update")
-    connection.commit()
+    get_connection()
+    cursor.execute("""SELECT * from data WHERE std is null ORDER BY id""")
 
 
 def get_spec(index):
     """
     Using CallistoSpectrogram.read to read the path.
-
     Parameters
     ----------
     index : index of specific column to get the right path
-
-
     Returns
     -------
     A spectrogram
-
     """
     spec = CallistoSpectrogram.read(test_config.DATA_PATH + index[1])
     return spec
-
 
 def flatted_data(spec):
     """
@@ -44,34 +38,52 @@ def flatted_data(spec):
     Parameters
     ----------
     spec: A spectrogram.
-
     Returns
     -------
-    The absolute value of data.
+    The absolute value of the data.
     """
     spec2 = spec.subtract_bg("subtract_bg_sliding_window", window_width=800, affected_width=1,
                              amount=0.05, change_points=True)
     data = np.absolute(spec2.data.flatten())
     return data
 
+def calculate_values(index):
+    """
+    Calculate the std and snr, then update them into the table in Database.
+    Parameters
+    ----------
+    index : index of data in the cursor
+
+    Returns
+    -------
+    Update the values into DB.
+    """
+    spec = get_spec(index)
+    data = flatted_data(spec)
+    std_data = standard_deviation(data)
+    snr_data = signal_to_noise(data)
+    sql_update_query = f"""UPDATE data SET std = {std_data}, snr= {snr_data} where id = {index[0]} """
+    cursor.execute(sql_update_query)
+    connection.commit()
+
+
 
 def save_values_db():
     """
     Update the values (std and snr ) into the database (validation).
     """
-    global index, data, std_data, snr_data
+    get_cursor()
     for index in cursor.fetchall():
         try:
-            spec = get_spec(index)
-
-            print("Table before Update")
-            data = flatted_data(spec)
-
-            std_data = standard_deviation(data)
-            snr_data = signal_to_noise(data)
-            update_values()
+            calculate_values(index)
 
         except Exception as err:
+            print(err)
             exception_type = type(err).__name__
             print(exception_type, index[1])
+
+
+if __name__ == "__main__":
+    save_values_db()
+
 
