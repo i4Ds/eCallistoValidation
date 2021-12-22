473e51119927527c4a71bb4a43893f05bbf8624e
Delberin Ali <61394570+Delberin-Ali@users.noreply.github.com>
2021-12-22 21:29:17 +0100

    - removed some private infos

--- validation/configs/config.py
+++ validation/configs/config.py
@@ -13,11 +13,4 @@ DB_USER = 'postgres'
 DB_PASSWORD = 'ecallistohackorange'
 DB_PORT = '5432'
 
-"""
-DB_HOST = 'localhost'
-DB_DATABASE = 'temp'
-DB_USER = 'postgres'
-DB_PASSWORD = 'ecallistohackorange'
-DB_PORT = '5432'
-"""
 
