a5b83475c13dd46f54af9f4521050efe0fe660fc
Delberin Ali <61394570+Delberin-Ali@users.noreply.github.com>
2022-01-12 12:39:41 +0100

    - removed some infos about the DB

--- validation/config.py
+++ validation/config.py
@@ -2,15 +2,3 @@ import os
 
 PATH_PREFIX = '/data'
 DATA_PATH = os.path.join(PATH_PREFIX, 'radio/2002-20yy_Callisto')
-
-
-
-# database
-
-DB_HOST = 'localhost'
-DB_DATABASE = 'validation'
-DB_USER = 'postgres'
-DB_PASSWORD = 'ecallistohackorange'
-DB_PORT = '5432'
-
-
