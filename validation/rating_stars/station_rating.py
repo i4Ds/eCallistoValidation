b79fd33f29cd8689c3ccb4467cbabcc872eb3645
Delberin Ali <61394570+Delberin-Ali@users.noreply.github.com>
2022-06-29 14:20:47 +0200

    - removed some necessary code

--- validation/rating_stars/station_rating.py
+++ validation/rating_stars/station_rating.py
@@ -6,16 +6,14 @@ import psycopg2
 import pandas as pd
 import pandas.io.sql as psql
 import matplotlib.pyplot as plt
-from validation import  get_database
 sys.path.append('../')
 import config as validation_config
-
-
 
 
 # 5 stars means best station
 # 1 star means bad station
 def get_db():
+    """ connect to the database"""
     database = psycopg2.connect(host=validation_config.DB_HOST,
                                 user=validation_config.DB_USER,
                                 database=validation_config.DB_DATABASE,
@@ -23,7 +21,9 @@ def get_db():
                                 password=validation_config.DB_PASSWORD)
     return database
 
+
 def get_all_instruments(database, sql_query):
+    """ Get the all data from the database"""
 
     sql_query_instruments = sql_query
     cursor = database.cursor(cursor_factory=psycopg2.extras.DictCursor)
@@ -34,10 +34,11 @@ def get_all_instruments(database, sql_query):
 
 
 def rating_with_files():
+    """ To evaluate the files according to the file names"""
 
     sql_query = "select * from validation_data where snr_values is not null order by snr_values desc ;"
     database = get_db()
-    rows, cursor = get_database( database, sql_query )
+    rows = get_all_instruments( database, sql_query )
 
     while True:
         name = input( "Enter the file name: " )
@@ -49,50 +50,35 @@ def rating_with_files():
                     print( f"The file name {name} got {row[9]*'*'}")
 
 
-rating_with_files()
+#rating_with_files()
 
 
-
-
-
-def stations_with_stars(start_time, end_time):
+def files_with_stars(start_time, end_time):
+    """To get  a list of filenames evaluated with stars"""
+    database = get_db()
     cursor = database.cursor()
     sql_query ="select file_name, snr_rating from validation_data where start_time between '%s' AND '%s' order by snr_rating desc ;" % (start_time, end_time)
     pd.set_option('display.max_rows', None)
     df = pd.read_sql_query(sql_query , database )
 
-    return df
+    print(df)
 
+#files_with_stars('2017-09-06 09:15:00','2017-09-06 09:30:00')
 
-#print(stations_with_stars('2017-09-06 09:15:00','2017-09-06 09:30:00'))
-
-def avg_stations(start_time, end_time):
-    database = psycopg2.connect(host=validation_config.DB_HOST,
-                                user=validation_config.DB_USER,
-                                database=validation_config.DB_DATABASE,
-                                port=validation_config.DB_PORT,
-                                password=validation_config.DB_PASSWORD)
+def avg_stations_with_stars(start_time, end_time):
+    database = get_db()
     cursor = database.cursor()
     sql_query ="select instrument_name, avg(snr_rating) as avg_rating  from validation_data where start_time between '%s' AND '%s' group by instrument_name order by avg_rating desc ;" % (start_time, end_time)
     pd.set_option('display.max_rows', None)
     df = pd.read_sql_query(sql_query , database )
     df["avg_rating"] = df["avg_rating"].round( decimals=0 )
 
-    return df
+    print(df)
 
-#print(avg_stations('2017-09-01 09:15:00','2017-09-03 09:30:00'))
-
+#avg_stations_with_stars('2017-09-01 09:15:00','2017-09-03 09:30:00')
 
 def rating_with_stations(name):
-
-    database = psycopg2.connect(host=validation_config.DB_HOST,
-                                user=validation_config.DB_USER,
-                                database=validation_config.DB_DATABASE,
-                                port=validation_config.DB_PORT,
-                                password=validation_config.DB_PASSWORD)
-    # cursor = database.cursor()
-
-
+    database = get_db()
     sql_query = "SELECT instrument_name , avg(snr_rating) as avg_rating from validation_data group by instrument_name, snr_rating order by snr_rating desc;"
     pd.set_option('display.max_rows', None)
     df = pd.read_sql_query(sql_query , database )
@@ -103,5 +89,4 @@ def rating_with_stations(name):
             print( f"The station {row[0]} got {row[1]} star" )
 
 
-
-# rating_with_stations("AUSTRIA")
+#rating_with_stations("AUSTRIA")
