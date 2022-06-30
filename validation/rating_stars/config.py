958e3cd4a427882ddc6bbd9ca0cb5a34533a26ae
Delberin Ali <61394570+Delberin-Ali@users.noreply.github.com>
2022-06-30 13:55:08 +0200

    - changed the db name

--- validation/rating_stars/config.py
+++ validation/rating_stars/config.py
@@ -6,8 +6,8 @@ DATA_PATH = os.path.join(PATH_PREFIX, 'radio/2002-20yy_Callisto')
 #database
 DB_HOST='localhost'
 #DB_HOST='10.35.147.65'
-#DB_DATABASE='validation'
-DB_DATABASE='ecallisto'
+DB_DATABASE='validation'
+#DB_DATABASE='ecallisto'
 DB_USER='postgres'
-DB_PASSWORD='1234'
+DB_PASSWORD='ecallistohackorange'
 DB_PORT='5432'
