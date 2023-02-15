0d58894713c211b8c9c8d241c6218a7c64ab5f36
Delberin-Ali <delberin@Dilo.localdomain>
2023-02-15 12:28:27 +0100

    - created a class called Rating for the rating system

--- validation/rating_stars/rating.py
+++ validation/rating_stars/rating.py
@@ -1,95 +1,76 @@
 import sys
-import astropy.io.fits
 import numpy as np
 import psycopg2.extras
 import psycopg2
 import pandas as pd
 import pandas.io.sql as psql
 import matplotlib.pyplot as plt
-import re
-import datetime
-import math
-import mpu
+
 import config as ecallisto_config
 import os
-import skyfield
+
 
 sys.path.append(os.path.join(os.path.dirname(__file__), "../..", "radiospectra2"))
 from radiospectra.sources import CallistoSpectrogram
 from collections import Counter
 
 
-# 5 stars means best station
-# 1 star means bad station
+class Rating:
+    def __init__(self, start_time, end_time):
+        self.start_time = start_time
+        self.end_time = end_time
+        self.database = self.get_db()
 
+    def get_db(self):
+        """Connect to the database"""
+        database = psycopg2.connect(host=ecallisto_config.DB_HOST,
+                                    user=ecallisto_config.DB_USER,
+                                    database=ecallisto_config.DB_DATABASE,
+                                    port=ecallisto_config.DB_PORT,
+                                    password=ecallisto_config.DB_PASSWORD)
+        return database
 
-def get_db() :
-    """ connect to the database"""
-    database = psycopg2.connect( host=ecallisto_config.DB_HOST,
-                                 user=ecallisto_config.DB_USER,
-                                 database=ecallisto_config.DB_DATABASE,
-                                 port=ecallisto_config.DB_PORT,
-                                 password=ecallisto_config.DB_PASSWORD )
-    return database
+    def get_all_instruments(self):
+        """Get all data from the database"""
+        sql_query_instruments = f"SELECT path, snr, std FROM test WHERE start_time BETWEEN '{self.start_time}' AND '{self.end_time}' AND snr IS NOT NULL ORDER BY snr DESC;"
+        cursor = self.database.cursor(cursor_factory=psycopg2.extras.DictCursor)
+        cursor.execute(sql_query_instruments)
+        instruments = [row for row in cursor.fetchall()]
+        return instruments
 
+    def convert_to_stars(self, score):
+        ranking = [1.0, 2.0, 3.0, 4.0, 5.0]
 
-def get_all_instruments(database, sql_query):
-    """ Get the all data from the database"""
+        # Compute the percentile of the data
+        percent = np.percentile(score, [0, 25, 50, 75, 100])
+        rating = np.interp(score, percent, ranking)
+        rating = np.round(rating, 2)
 
-    sql_query_instruments = sql_query
-    cursor = database.cursor(cursor_factory=psycopg2.extras.DictCursor)
-    cursor.execute(sql_query_instruments)
-    instruments = [row for row in cursor.fetchall()]
+        return rating
 
-    return instruments
+    def rate_stations(self):
+        instruments = self.get_all_instruments()
 
+        df = pd.DataFrame(instruments, columns=["path", "snr", "std"])
 
-#get_all_instruments(database)
+        df = df[df['snr'].notna()]
 
-# this function convert the values to ranking system between 1 to 5
-def convert_to_stars(score):
-    ranking = [1.0, 2.0, 3.0, 4.0, 5.0]
+        df["station_name"] = df["path"].str.split('[/|_]', expand=True)[4]
 
-    # Compute the percentile of the data
-    percent = np.percentile( score, [0, 25, 50, 75, 100] )
-    rating = np.interp( score, percent, ranking )
-    rating = np.round(rating, 2)
+        df["snr_rating"] = self.convert_to_stars(df["snr"])
+        df["std_rating"] = self.convert_to_stars(df["std"])
 
-    return rating
+        df["counter"] = df["station_name"].tolist()
+        values, counts = np.unique(df["counter"], return_counts=True)
 
-## this rating function uses the quantile normalization to sort the data into different bins =>  every bin represents a certain rating (between  1 and  5)
-def rate_stations(start_time, end_time):
+        df = df.groupby('station_name').mean().round(decimals=2).sort_values(by='snr_rating', ascending=False)
+        df["number_of_files"] = counts
 
-    database = get_db()
-    database.cursor()
-    sql_query = "Select path, snr,std from test where start_time between '%s' And '%s' and  snr is not null order by snr desc;" % (start_time, end_time)
-
-
-    pd.set_option('display.max_rows', None)
-    df = pd.read_sql_query(sql_query , database )
-    df = df[df['snr'].notna()]
-
-    df["station_name"] = df["path"].str.split( '[/|_]', expand=True )[4]
-
-    df["snr_rating"] = convert_to_stars( df["snr"] )
-    df["std_rating"] = convert_to_stars( df["std"] )
-
-    df["counter"] = df["station_name"].tolist()
-    values, counts = np.unique( df["counter"], return_counts=True )
-
-    df = df.groupby( 'station_name' ).mean().round( decimals=2 ).sort_values( by='snr_rating', ascending=False )
-    #df["snr_stars"] = [math.ceil( star ) * "*" for star in df["snr_rating"]]
-    #df["std_stars"] = [math.ceil( star ) * "*" for star in df["std_rating"]]
-    df["number_of_files"] = counts
-
-    print(df[["snr","snr_rating", "std", "std_rating" , "number_of_files"]])
+        return df[["snr", "snr_rating", "std", "std_rating", "number_of_files"]]
 
 
 
 if __name__ == '__main__':
-
-    rate_stations('2021-09-08 08:00:00', '2021-09-08 18:45:00')
-
-
-
-
+    rating = Rating('2021-09-08 08:00:00', '2021-09-08 18:45:00')
+    df = rating.rate_stations()
+    print(df)
