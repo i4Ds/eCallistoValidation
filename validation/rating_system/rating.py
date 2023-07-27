89b3d5fb92b9aed268d1b459b7dfd2e0f9b809fc
Delberin <delberin.ali@fhnw.ch>
2023-07-27 14:00:08 +0200

    refactored !!

--- validation/rating_system/rating.py
+++ validation/rating_system/rating.py
@@ -1,17 +1,14 @@
+import os
 import sys
+
 import numpy as np
-import psycopg2.extras
-import psycopg2
 import pandas as pd
-import pandas.io.sql as psql
-import matplotlib.pyplot as plt
+import psycopg2
+import psycopg2.extras
 
 import config as ecallisto_config
-import os
 
 sys.path.append(os.path.join(os.path.dirname(__file__), "../..", "radiospectra2"))
-from radiospectra.sources import CallistoSpectrogram
-from collections import Counter
 
 
 class Rating:
@@ -27,37 +24,33 @@ class Rating:
         self.end_time = end_time
         self.database = self.get_db()
 
-
-
-    def get_db(self):
+    @staticmethod
+    def get_db():
         """
         Get the database connection
         :return: The database connection
         """
-        database = psycopg2.connect( host=ecallisto_config.DB_HOST,
-                                     user=ecallisto_config.DB_USER,
-                                     database=ecallisto_config.DB_DATABASE,
-                                     port=ecallisto_config.DB_PORT,
-                                     password=ecallisto_config.DB_PASSWORD )
+        database = psycopg2.connect(host=ecallisto_config.DB_HOST,
+                                    user=ecallisto_config.DB_USER,
+                                    database=ecallisto_config.DB_DATABASE,
+                                    port=ecallisto_config.DB_PORT,
+                                    password=ecallisto_config.DB_PASSWORD)
         return database
-    
-
 
     def get_all_instruments(self):
         """
         Get all instruments from the database
         :return: The instruments
         """
-        sql_query_instruments = f"SELECT path, snr, std, lower(observation_times) AS start_time, upper(observation_times) AS end_time FROM fits WHERE observation_times && tsrange('{start_time}', '{end_time}') AND snr IS NOT NULL ORDER BY snr DESC;"
+        sql_query_instruments = f"SELECT path, snr, std, lower(observation_times) AS start_time, upper(observation_times) AS end_time FROM fits WHERE observation_times && tsrange('{self.start_time}', '{self.end_time}') AND snr IS NOT NULL ORDER BY snr DESC;"
 
-        cursor = self.database.cursor( cursor_factory=psycopg2.extras.DictCursor )
-        cursor.execute( sql_query_instruments )
+        cursor = self.database.cursor(cursor_factory=psycopg2.extras.DictCursor)
+        cursor.execute(sql_query_instruments)
         instruments = [row for row in cursor.fetchall()]
         return instruments
-    
 
-
-    def convert_to_stars(self, score):
+    @staticmethod
+    def convert_to_stars(score):
         """
         Convert the score to a rating.
         :param score: The score
@@ -66,13 +59,11 @@ class Rating:
         ranking = [1.0, 2.0, 3.0, 4.0, 5.0]
 
         # Compute the percentile of the data
-        percent = np.percentile( score, [0, 25, 50, 75, 100] )
-        rating = np.interp( score, percent, ranking )
-        rating = np.round( rating, 2 )
+        percent = np.percentile(score, [0, 25, 50, 75, 100])
+        rating = np.interp(score, percent, ranking)
+        rating = np.round(rating, 2)
 
         return rating
-    
-
 
     def rate_stations(self):
         """
@@ -81,30 +72,34 @@ class Rating:
         """
         instruments = self.get_all_instruments()
 
-        df = pd.DataFrame( instruments, columns=["path", "snr", "std", "start_time", "end_time"] )
+        df = pd.DataFrame(instruments, columns=["path", "snr", "std", "start_time", "end_time"])
 
         df = df[df['snr'].notna()]
 
-        df["station_name"] = df["path"].str.split( '[/|_]', expand=True )[4]
+        df["station_name"] = df["path"].str.split('[/|_]', expand=True)[4]
 
-        df["snr_rating"] = self.convert_to_stars( df["snr"] )
-        df["std_rating"] = self.convert_to_stars( df["std"] )
+        df["snr_rating"] = self.convert_to_stars(df["snr"])
+        df["std_rating"] = self.convert_to_stars(df["std"])
 
         df["counter"] = df["station_name"].tolist()
-        values, counts = np.unique( df["counter"], return_counts=True )
+        values, counts = np.unique(df["counter"], return_counts=True)
 
-        df = df.groupby( 'station_name' ).mean().round( decimals=2 ).sort_values( by='snr_rating', ascending=False )
-        df["number_of_files"] = counts
+        df = df.groupby('station_name', as_index=False).agg({
+            'snr': 'mean',
+            'std': 'mean',
+            'snr_rating': 'mean',
+            'std_rating': 'mean'
+        }).round(decimals=2).sort_values(by='snr_rating', ascending=False)
 
-        return df[["snr", "snr_rating", "std", "std_rating", "number_of_files"]]
+        df.columns = ['station_name', 'snr', 'std', 'snr_rating', 'std_rating']
 
-
+        return df
 
 
 if __name__ == '__main__':
-    start_time = input( "Enter start time (YYYY-MM-DD HH:MM:SS): " )
-    end_time = input( "Enter end time (YYYY-MM-DD HH:MM:SS): " )
+    start_time = input("Enter start time (YYYY-MM-DD HH:MM:SS): ")
+    end_time = input("Enter end time (YYYY-MM-DD HH:MM:SS): ")
 
-    rating = Rating( start_time, end_time )
+    rating = Rating(start_time, end_time)
     df = rating.rate_stations()
-
+    print(df)
